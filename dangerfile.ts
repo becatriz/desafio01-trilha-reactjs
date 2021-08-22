9b64cc5b56280fd2cca5efbf1d5556b6d8ce8714
Rebeca Lopes <rebeca.cruz@before.com.br>
2021-08-22 16:51:58 -0300

    fix: adiciona helpers

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,12 +1,22 @@
 import { danger, fail, warn, message, markdown } from "danger";
 
+
 const componentsFiles = danger.git.fileMatch("./src/components/**");
 const componentsTestFiles = danger.git.fileMatch(
   "./src/__tests__/components/**"
 );
 
+const helperFiles = danger.git.fileMatch("./src/helpers/*.ts");
+const helperTestFiles = danger.git.fileMatch("./src/helpers/*.spec.ts");
+
 if (componentsFiles.created && !componentsTestFiles.created) {
   warn(
     "Ops! Foi criado um UI components novo, mas não foi criado o teste para ele 😓"
+  );
+}
+
+if (helperFiles.edited && !helperTestFiles.edited) {
+  fail(
+    "Ops! Foi criado um novo helper, mas não foi criado o teste para ele 😓"
   );
 }
